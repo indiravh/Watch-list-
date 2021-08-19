a985c4d806fc98044c4b858ddfce08698a9ab435
Indira Vanhulle <indira_vanhulle@hotmail.com>
2021-08-19 17:27:38 +0200

    finish exercise

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    # we need @list in our `simple_form_for`
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # we need `list_id` to associate bookmark with corresponding list
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+     if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new # for errors of validation. With render :new, unlike redirect_to, you can show a new page again without reset of form values. It's pretty useful in the aspect of UX.
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmark_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id) #
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'json'
+require 'open-uri'
+
+# # If I want to Parse the API's images, use text below 👇 and replace poster_url:
+  # base_url = 'http://tmdb.lewagon.com/movie/top_rated'
+  # user_serialized = URI.open(base_url).read
+  # user = JSON.parse(user_serialized)
+
+Movie.create(title: Faker::Movie.title, overview: Faker::Quote.famous_last_words, poster_url: "https://image.tmdb.org/t/p/w500//loRmRzQXZeqG78TqZuyvSlEQfZb.jpg" , rating: rand(11))
+Movie.create(title: Faker::Movie.title, overview: Faker::Quote.famous_last_words, poster_url: "https://image.tmdb.org/t/p/w500//5ZjMNJJabwBEnGVQoR2yoMEL9um.jpg" , rating: rand(11))
+Movie.create(title: Faker::Movie.title, overview: Faker::Quote.famous_last_words, poster_url: "https://image.tmdb.org/t/p/w500//poec6RqOKY9iSiIUmfyfPfiLtvB.jpg" , rating: rand(11))
+Movie.create(title: Faker::Movie.title, overview: Faker::Quote.famous_last_words, poster_url: "https://image.tmdb.org/t/p/w500//hek3koDUyRQk7FIhPXsa6mT2Zc3.jpg" , rating: rand(11))
